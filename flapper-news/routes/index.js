808c50accf18be3d7ab608826fee95c32a51b6d5
Dibble <officer.dibble1@gmail.com>
2014-12-11 22:43:55 +0000

    adding upvote functionality to backend

--- flapper-news/routes/index.js
+++ flapper-news/routes/index.js
@@ -15,10 +15,41 @@ router.get('/posts', function(req, res, next) {
 	});
 });
 
+router.get('/posts/:post', function(req, res) {
+	res.json(req.post);
+});
+
 router.post('/posts', function(req, res, next) {
 	var post = new Post(req.body);
 
 	post.save(function(err, post) {
+		if (err) {
+			return next(err);
+		}
+
+		res.json(post);
+	});
+});
+
+router.param('post', function(req, res, next, id) {
+	var query = Post.findById(id);
+
+	query.exec(function(err, post) {
+		if (err) {
+			return next(err);
+		}
+
+		if(!post) {
+			return next(new Error('No post with that ID found'));
+		}
+
+		req.post = post;
+		return next();
+	});
+});
+
+router.put('/posts/:post/upvote', function(req, res, next) {
+	req.post.upvote(function(err, post) {
 		if (err) {
 			return next(err);
 		}
